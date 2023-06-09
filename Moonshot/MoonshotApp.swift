c553a25890226b956630094587791d5a39857364
fauzandp <fauzan27dp@gmail.com>
2023-06-09 14:58:21 +0700

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Fauzan Dwi Prasetyo on 09/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
